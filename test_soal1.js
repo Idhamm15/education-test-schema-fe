9f522a7c6eb30906621b2772c0fc1a09b58ec53d
Idhamm15 <idhamtamvanz123@gmail.com>
2022-07-12 19:24:13 +0700

    Mohamad Idham Bahri : Test Kawah Edukasi bath III

--- test_soal1.js
+++ test_soal1.js
@@ -1,25 +1,36 @@
 //buatlah sebuah program untuk menampilkan banyak angka yang keluar
 //output yang di inginkan : total data paling banyak keluar adalah #angka dengan jumlah #10
 
+const mode = arr => {
+    const mode = {};
+    let max = 0, count = 0;
+  
+    for(let i = 0; i < arr.length; i++) {
+      const item = arr[i];
+
+      
+      
+      if(mode[item]) {
+        mode[item]++;
+      } else {
+        mode[item] = 1;
+      }
+      
+      if(count < mode[item]) {
+        max = item;
+        count = mode[item];
+      }
+    }
+    
+    
+    return `total data paling banyak keluar adalah ${max} dengan jumlah ${count}`;
+  };
+
 var data1 = [1, 1, 1, 2, 2, 4, 1, 1 ]
+console.log(mode([1, 1, 1, 2, 2, 4, 1, 1 ]));
+
 var data2 = [2, 1, 2, 2, 2, 8, 1, 1 ]
+console.log(mode([2, 1, 2, 2, 2, 8, 1, 1 ]));
+
 var data3 = [3, 3, 3, 3, 2, 4, 1, 1 ]
-
-
-var filteredArray = data1.filter(function (x) {
-    return Math.max
-    console.log ("Total data paling banyak ", Math.max, "Dengan Jumlah", math)
-})
-
-var filteredArray2 = data2.filter(function (x) {
-    return Math.max
-    console.log ("Total data paling banyak ", Math.max, "Dengan Jumlah", math
-})
-
-var filteredArray3 = data3.filter(function (x) {
-    return Math.max
-    console.log ("Total data paling banyak ", Math.max, "Dengan Jumlah", math
-})
-
-
-
+console.log(mode([3, 3, 3, 3, 2, 4, 1, 1 ]))
--- test_soal2.js
+++ test_soal2.js
@@ -3,38 +3,19 @@
 //- Jika inputan mengandung angka maka harus mengeluarkan output : Sistem kami menolak untuk inputan berisi angka 167283
 //- Jika inputan tidak mengandung angka maka harus mengeluarkan output : Sistem memeriksa data anda valid dengan ketentuan kami dengan inputan hallo justin selamat datang!
 
-
 var input1 = "hallo jesika24 selamat datang!"
 var input2 = "hallo anggun selamat datang!"
 var input3 = "hallo ** selamat datang!"
 var input4 = "hallo Mariage889120! selamat datang!"
 
-
-var Validasi1 = input1.includes ({
-   if (array == string) {
-      console.log ('Sistem memeriksa data anda valid dengan ketentuan kami dengan inputan hallo justin selamat datang!
-      ')
-   }else {
-      console.log ('Sistem kami menolak untuk inputan berisi angka 167283')
-   }
-
-
-
-var Validasi2 = input2.includes ({
-   if (array == string) {
-      console.log ('Sistem memeriksa data anda valid dengan ketentuan kami dengan inputan hallo justin selamat datang!
-         ')
-   }else {
-      console.log ('Sistem kami menolak untuk inputan berisi angka 167283')
-   } 
-
-
-   
-var Validasi3 = input3.includes ({
-   if ( == string) {
-      console.log ('Sistem memeriksa data anda valid dengan ketentuan kami dengan inputan hallo justin selamat datang!
-      ')
-   }else {
-      console.log ('Sistem kami menolak untuk inputan berisi angka 167283')
-   }
-   
+function ValidasiHuruf(input){
+    if(input.match(/^-{0,1}\D+$/)){
+        console.log('Sistem memeriksa data anda valid dengan ketentuan kami dengan inputan hallo justin selamat datang!')
+    }else{
+        console.log('Sistem kami menolak untuk inputan berisi angka 167283');
+    }
+}
+ValidasiHuruf(input1)
+ValidasiHuruf(input2)
+ValidasiHuruf(input3)
+ValidasiHuruf(input4)
